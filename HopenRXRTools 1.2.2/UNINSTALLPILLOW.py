d1c58def5b642a4edcf3734db40802246ba4ab7c
Hopen111 <147209436+Hopen111@users.noreply.github.com>
2024-12-04 17:48:02 -0600

    Delete HopenRXRTools 1.2.2 directory

--- HopenRXRTools 1.2.2/UNINSTALLPILLOW.py
+++
@@ -1,5 +0,0 @@
-input("Press Enter To Begin Uninstallation." )
-import sys
-import subprocess
-# implement pip as a subprocess:
-subprocess.check_call([sys.executable, '-m', 'pip', 'uninstall', 'pillow'])
